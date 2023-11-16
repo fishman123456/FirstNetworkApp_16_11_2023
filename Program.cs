cbf334233bed2aaaf56c9f3af32bce258d9c7e68
fishman123456 <den9152@yandex.ru>
2023-11-17 00:49:38 +0700

    отправка - получение сообщения

--- Program.cs
+++ Program.cs
@@ -11,33 +11,62 @@ namespace FirstNetworkApp_16_11_2023
 {
     internal class Program
     {
-
-     static void RunServer(string serverIpStr, int serverPort)
+        static void RunServer(string serverIpStr, int serverPort)
         {
             // 0 конфигурация сервера
             //string serverIpStr = "25.45.70.14"; // ip адрес сокета сервера
             //int serverPort = 2620; // порт сокета сервера
-            // 1 подготовить endpoint для работы сервера 
-            IPAddress serverIp = IPAddress.Parse(serverIpStr);
-            IPEndPoint serverEndpoint = new IPEndPoint(serverIp, serverPort);
-            // 2 создать сокет сервера и присоединить его к endpoint
-            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            server.Bind(serverEndpoint); // связывание с сервером и подключение к нему
-            // 3 перевести сокет в режим прослушивания входящих подключений
-            server.Listen(1);
-            // 4 начать ожидание входящего подключения
-            Console.WriteLine("server>Ожидание входящего подключения ....");
-            Socket client = server.Accept();
-            Console.WriteLine($"server>Произошло подключение: {client.RemoteEndPoint}");
-            Console.ReadLine();
-            // 5 далее осуществляется работа с подключеным клиентом telnet 25.41.112.239 1024   ------telnet  25.45.70.14 2620
-            // отправим сообщение клиенту
-            //string message = $"Helloy from server, connections time {DateTime.Now}";
-            //client.Send(Encoding.ASCII.GetBytes(message));
-            //Console.WriteLine($"отправлено сообщение {message}");
-            //Console.ReadLine();
-            Console.WriteLine("server>Завершение работы сервера...");
-            Console.ReadLine();
+            Socket server = null;
+            Socket client = null;
+            try
+            {
+                // 1 подготовить endpoint для работы сервера 
+                IPAddress serverIp = IPAddress.Parse(serverIpStr);
+                IPEndPoint serverEndpoint = new IPEndPoint(serverIp, serverPort);
+                // 2 создать сокет сервера и присоединить его к endpoint
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                server.Bind(serverEndpoint); // связывание с сервером и подключение к нему
+                                             // 3 перевести сокет в режим прослушивания входящих подключений
+                server.Listen(1);
+                // 4 начать ожидание входящего подключения
+                Console.WriteLine("server>Ожидание входящего подключения ....");
+                client = server.Accept();
+                Console.WriteLine($"server>Произошло подключение: {client.RemoteEndPoint}");
+                Console.ReadLine();
+                #region
+                // 5 далее осуществляется работа с подключеным клиентом telnet 25.41.112.239 1024   ------telnet  25.45.70.14 2620
+                // отправим сообщение клиенту
+                //string message = $"Helloy from server, connections time {DateTime.Now}";
+                //client.Send(Encoding.ASCII.GetBytes(message));
+                //Console.WriteLine($"отправлено сообщение {message}");
+                //Console.ReadLine();
+                #endregion
+
+                // 5.1 общение с клиентом
+                string message = $"Вы подключились, время подключения {DateTime.Now}";
+                client.Send(Encoding.UTF8.GetBytes(message));
+                Console.WriteLine($"server> отправлено сообщение {message}");
+                // 5.2  получить сообщение от клиента
+                byte[] buffer = new byte[1024];
+                int bytesRead = client.Receive(buffer);
+                message = Encoding.UTF8.GetString(buffer,0,bytesRead);
+                Console.WriteLine($"server> получено сообщение   {message}");
+
+                Console.WriteLine("server>Завершение работы сервера...");
+                // завершение работы склиента
+                client.Shutdown(SocketShutdown.Both);
+                Console.WriteLine("client> Завершение работы клиента");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"При работе сервера возникло исключение {ex.Message}");
+            }
+            finally
+            {
+                server?.Close();
+                client?.Close();
+            }
         }
         // Процедура запуска линейного алгоритма работы клиента (активного сокета)
         static void RunClient(string serverIpStr, int serverPort)
@@ -45,33 +74,61 @@ namespace FirstNetworkApp_16_11_2023
             // СОЗДАДИМ ПРОСТЕЙШИЙ КЛИЕНТ
             // Клиент инициирует подключение к серверу
             // Вывод сообщение о том, что произошло подключение
-
+            Socket client = null;
             // ХОД РАБОТЫ:
-            
-            // 1. Создадим сокет клиента 
-            Socket client = new Socket (AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
-            
-            // 2. Подготовить endpoint  сервера
-            IPAddress serverIp = IPAddress.Parse(serverIpStr);
-            IPEndPoint serverEndpoint = new IPEndPoint(serverIp, serverPort);
+            try
+            {
+                // 1. Создадим сокет клиента 
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            // 3. Подключиться к серверу
-            Console.WriteLine("client>Подключение к серверу...");
-            client.Connect(serverEndpoint);
-            Console.WriteLine($"client>Произошло подключение ...{client.LocalEndPoint}-> {client.RemoteEndPoint}");
-            Console.WriteLine("client>Завершение работы клиента...");
-            Console.ReadLine();
+                // 2. Подготовить endpoint  сервера
+                IPAddress serverIp = IPAddress.Parse(serverIpStr);
+                IPEndPoint serverEndpoint = new IPEndPoint(serverIp, serverPort);
+
+                // 3. Подключиться к серверу
+                Console.WriteLine("client>Подключение к серверу...");
+                client.Connect(serverEndpoint);
+                Console.WriteLine($"client>Произошло подключение ...{client.LocalEndPoint}-> {client.RemoteEndPoint}");
+                
+                // 4.1 получить сообщение от сервера
+                byte[]buffer = new byte[1024]; // буфер для чтения сообщения
+                int byteRead = client.Receive(buffer); // читам сообщения в буфер, результат кол-во байт
+                string message = Encoding.UTF8.GetString(buffer,0,byteRead); // декодируем байты в строку
+
+                // 4.2 отправить сообщение серверу
+                message = "Отлично, работаем дальше";
+                client.Send(Encoding.UTF8.GetBytes((string)message));
+
+                Console.WriteLine($"получено сообщение {message}");
+                Console.WriteLine("client>Завершение работы клиента...");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"При работе клиента возникло исключение {ex.Message}");
+            }
+            finally
+            {
+                client?.Close();
+            }
         }
         static void Main(string[] args)
         {
+            // Алгоритм общения (линейный):
+            // 1) После установки подключения сервер отправляет клиенту сообщение о том, что произошло подключение и время подключения
+            // 2) Клиент отправляет серверу сообщение
+            // 3) Сервер отправляет клиенту сообщение Goodbye, работа завершается
             // Запустим сервер и клиент в разных потоках
             string serverIpStr = "25.45.70.14"; // ip адрес сокета сервера
             int serverPort = 2620; // порт сокета сервера
             Thread serverThread = new Thread(() => RunServer(serverIpStr, serverPort));
             Thread clientThread = new Thread(() => RunClient(serverIpStr, serverPort));
+
+            // запустить потоки
             serverThread.Start();
             clientThread.Start();
 
+            // дождаться завершение потока
             serverThread.Join();
             clientThread.Join();
         }
